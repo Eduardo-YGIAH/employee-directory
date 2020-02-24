89b5555cab4a309d60558e2cfe520d566fab9ba4
Eduardo Neto <edu.neto@icloud.com>
2020-02-24 15:00:55 +0000

    set github pages deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import TableSorted from './components/TableSorted';
 import HeaderBlock from './components/HeaderBlock';
 import UserDetails from './components/UserDetails';
